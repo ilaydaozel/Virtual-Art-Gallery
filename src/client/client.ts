890a1546288ee7c25ad4447c3e14968fa0d323a2
ilaydaozel <ilaydaoozel@gmail.com>
2023-04-27 21:09:39 +0300

    Add artwork examples

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
-import * as glob from 'glob'
+import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
 
@@ -13,9 +13,10 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 0
 camera.position.y = 5
 
+
 //Light
 //Ambient Light
-let ambientLight = new THREE.AmbientLight(0xffffff, 1.0); //color, intensity, distance, decay
+let ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.8); //color, intensity, distance, decay
 ambientLight.position.x = camera.position.x; //light follows camera
 ambientLight.position.y = camera.position.y;
 ambientLight.position.z = camera.position.z;
@@ -63,6 +64,62 @@ floor.rotation.x = Math.PI / 2; //90 degrees
 scene.add(floor)
 
 
+const artworks = ['a', 'b', 'c']
+const artworkPlanes = []
+let ZCount = -10;
+
+
+const light = new THREE.SpotLight(0xFF000F, 1, 1, 1, 0.8, 1)
+scene.add(light)
+//renderer.shadowMap.enabled = true
+//renderer.shadowMap.type = THREE.PCFSoftShadowMap
+light.position.x = -15
+light.position.z = 0
+light.position.y = 20
+
+const data = {
+    color: light.color.getHex(),
+    mapsEnabled: true,
+    //shadowMapSizeWidth: 512,
+    //shadowMapSizeHeight: 512,
+}
+
+const gui = new GUI()
+const lightFolder = gui.addFolder('THREE.Light')
+lightFolder.addColor(data, 'color').onChange(() => {
+    light.color.setHex(Number(data.color.toString().replace('#', '0x')))
+})
+lightFolder.add(light, 'intensity', 0, 1, 0.01)
+
+const spotLightFolder = gui.addFolder('THREE.SpotLight')
+spotLightFolder.add(light, 'distance', 0, 100, 0.01)
+spotLightFolder.add(light, 'decay', 0, 4, 0.1)
+spotLightFolder.add(light, 'angle', 0, 1, 0.1)
+spotLightFolder.add(light, 'penumbra', 0, 1, 0.1)
+spotLightFolder.add(light.shadow.camera, "near", 0.1, 100).onChange(() => light.shadow.camera.updateProjectionMatrix())
+spotLightFolder.add(light.shadow.camera, "far", 0.1, 100).onChange(() => light.shadow.camera.updateProjectionMatrix())
+spotLightFolder.add(light.position, 'x', -50, 50, 0.01)
+spotLightFolder.add(light.position, 'y', -50, 50, 0.01)
+spotLightFolder.add(light.position, 'z', -50, 50, 0.01)
+spotLightFolder.open()
+
+
+for (let i = 0; i < artworks.length; i++) {
+
+    const planeGeometry = new THREE.BoxGeometry(7, 7, 0.3)
+    const artTexture = new THREE.TextureLoader().load('img/artwork/' + artworks[i] + '.jpg')
+    const artworkMaterial = new THREE.MeshPhongMaterial()
+    artworkMaterial.map = artTexture
+    const artwork = new THREE.Mesh(planeGeometry, artworkMaterial)
+    artwork.position.x = -19;
+    artwork.rotation.y = Math.PI / 2;
+    artwork.position.z = ZCount;
+    artwork.position.y = 7;
+    scene.add(artwork)
+    ZCount += 12;
+    artworkPlanes.push(artwork)
+}
+
 
 //Create the walls
 const wallGroup = new THREE.Group(); //create a group to hold the walls
@@ -71,7 +128,7 @@ scene.add(wallGroup);
 //Front wall
 const frontWall = new THREE.Mesh(
     new THREE.BoxGeometry(40, 20, 0.001),
-    new THREE.MeshBasicMaterial({ color: '#f2f2f2' })
+    new THREE.MeshLambertMaterial({ color: '#f2f2f2' })
 )
 frontWall.position.z = -25;
 frontWall.position.y = 10;
@@ -89,7 +146,7 @@ leftWall.rotation.y = Math.PI / 2;
 //Right wall
 const rightWall = new THREE.Mesh(
     new THREE.BoxGeometry(50, 20, 0.001),
-    new THREE.MeshBasicMaterial({ color: '#f9f9f9' })
+    new THREE.MeshLambertMaterial({ color: '#f9f9f9' })
 )
 
 rightWall.position.x = 20;
